96e89e72c0a9453e76cbe0b6a2f9c5ecb79f7be3
Simon Willison <swillison@gmail.com>
2023-01-12 19:35:51 -0800

    pytest-httpx dependency, refs #1

--- setup.py
+++ setup.py
@@ -32,6 +32,6 @@ setup(
         openai-to-sqlite=openai_to_sqlite.cli:cli
     """,
     install_requires=["click", "httpx", "sqlite-utils>=3.28"],
-    extras_require={"test": ["pytest"]},
+    extras_require={"test": ["pytest", "pytest-httpx"]},
     python_requires=">=3.7",
 )
